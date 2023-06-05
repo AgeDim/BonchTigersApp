67c9757c9add17234b7d1faed8824ce7666b6f8f
AgeDim <proga778@gmail.com>
2023-06-06 02:18:50 +0300

    Initial commit, added Start Page

---
+++ android/app/src/main/kotlin/com/example/bonch_tigers_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bonch_tigers_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bonch_tigers_app/features/login/LoginPage.dart';
+import 'package:bonch_tigers_app/features/start/StartPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/startPage',
+      routes: {
+        '/startPage': (context) => const StartPage(),
+        '/loginPage': (context) => const LoginPage(),
+      },
+    ),
+  );
+}
